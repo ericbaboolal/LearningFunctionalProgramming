c7597d816777ff320905c2d6fd39a3c80167df77
Eric Baboolal <ebaboolal@issaonline.edu>
2020-11-16 16:29:53 -0800

    let's start over again

--- csharp/OptionTests.cs
+++ csharp/OptionTests.cs
@@ -13,54 +13,17 @@ namespace Functional
         public static Option<T> Filter<T>(this Option<T> input, Func<T, bool> f) =>
             // This will help you start out
             // 1) Remove this line
-            // None<T>()
+            None<T>()
             // 2) Uncomment the following and Fill in the blanks
-            
-            
-            //the Some case is going to take the value inside of Some
-            //the internal Some value is represented with t
-            input.Match(
-                Some: i => f(i) ? Some(i) : None<T>(),
-                None: () => None<T>()
-            )
+            // input.Match(
+            //     Some: i => ______,
+            //     None: () => ______
+            // )
             ;
 
         // Map :: Option<A> -> (A -> B) -> Option<B>
-        public static Option<B> Map<A,B>(this Option<A> input, Func<A, B> f) =>
-            // This will help you start out
-            // 1) Remove this line
-            // None<T>()
-            // 2) Uncomment the following and Fill in the blanks
-            
-            
-            //the Some case is going to take the value inside of Some
-            //the internal Some value is represented with t
-            //we want to convert the int value in T to a string
-            input.Match(
-                Some: t => Some<B>(f(t)),       
-                None: () => None<B>()
-            )
-            ;
 
         // Bind :: Option<A> -> (A -> Option<B>) -> Option<B>
-        public static Option<B> Bind<A,B>(this Option<A> input, Func<A, Option<B>> f ) =>
-
-
-            input.Match(
-                Some: t => f(t),
-                None: () => None<B>()
-
-            )
-            ;
-        public static Option<B> SelectMany<A,B>(this Option<A> input, Func<A, Option<B>> f ) =>
-
-
-            input.Match(
-                Some: t => f(t),
-                None: () => None<B>()
-
-            )
-            ;
 
     }
 }
@@ -87,33 +50,33 @@ namespace csharp
         {
             var result_evens = Some(2)
                             // Uncomment the following once you've implemented Filter
-                            .Filter(i => i % 2 == 0)
+                            // .Filter(i => i % 2 == 0)
                             ;
-            Assert.Equal(Some(2), result_evens);
+            // Assert.Equal(Some(2), result_evens);
         }
         [Fact]
         public void Filter_Odds()
         {
             var result_odds = Some(2)
                             // Uncomment the following once you've implemented Filter
-                            .Filter(i => i % 2 == 1)
+                            // .Filter(i => i % 2 == 0)
                             ;
-            Assert.Equal(None<int>(), result_odds);
+            // Assert.Equal(None<int>(), result_odds);
         }
         [Fact]
         public void Filter_None()
         {
             var always_true = None<string>()
                             // Uncomment the following once you've implemented Filter
-                            .Filter(i => true)
+                            // .Filter(i => true)
                             ;
-            Assert.Equal(None<string>(), always_true);
+            // Assert.Equal(None<string>(), always_true);
 
             var always_false = None<string>()
                             // Uncomment the following once you've implemented Filter
-                             .Filter(i => false)
+                            // .Filter(i => false)
                             ;
-            Assert.Equal(None<string>(), always_false);
+            // Assert.Equal(None<string>(), always_false);
         }
     }
     public class OptionMapTests
@@ -123,58 +86,33 @@ namespace csharp
         {
             var result = Some(1)
                             // Uncomment the following once you've implemented Map
-                            .Map(i => $"Value: {i.ToString()}")
+                            // .Map(i => $"Value: {i}")
                             ;
-            Assert.Equal(Some("Value: 1"), result);
+            // Assert.Equal(Some("Value: 1"), result);
         }
         [Fact]
         public void Map_None()
         {
             var result = None<string>()
                             // Uncomment the following once you've implemented Map
-                            .Map(i => $"Value: {i}")
+                            // .Map(i => $"Value: {i}")
                             ;
-            Assert.Equal(None<string>(), result);
+            // Assert.Equal(None<string>(), result);
         }
         [Fact]
         public void Map_Nested()
         {
             var result_even = Some(2)
                             // Uncomment the following once you've implemented Map
-                            .Map(i => i % 2 == 0 ? Some(i.ToString()) : None<string>())
+                            // .Map(i => i % 2 == 0 ? Some(i.ToString()) : None<string>())
                             ;
-            Assert.Equal(Some(Some("2")), result_even);
+            // Assert.Equal(Some(Some("2")), result_even);
 
             var result_odd = Some(1)
                             // Uncomment the following once you've implemented Map
-                            .Map(i => i % 2 == 0 ? Some(i.ToString()) : None<string>())
+                            // .Map(i => i % 2 == 0 ? Some(i.ToString()) : None<string>())
                             ;
-            Assert.Equal(Some(None<string>()), result_odd);
-        }
-        [Fact]
-        public void Map_Other()
-        {
-            var result_even = 
-                Some("2")
-                    .Map(i => OptionBindTests.ParseInt(i) )
-                    ;
-            Assert.Equal(Some(Some(2)), result_even);
-
-            var r1 = 
-                Some(2)
-                    .Map(i => i + 3 );
-            Assert.Equal(Some(5), r1);
-
-             var r2 = 
-                Some(2)
-                    .Map(i => None<int>() );
-            Assert.Equal(Some(None<int>()), r2);
-
-            var r3 = 
-                Some(2)
-                    .Bind(i => Some(i + 3) );
-            Assert.Equal(Some(5), r3);
-
+            // Assert.Equal(Some(None<string>()), result_odd);
         }
     }
 
@@ -185,72 +123,33 @@ namespace csharp
         {
             var result = Some(1)
                             // Uncomment the following once you've implemented Map
-                            .Bind(i => Some($"Value: {i}"))
+                            // .Bind(i => Some($"Value: {i}"))
                             ;
-            Assert.Equal(Some("Value: 1"), result);
+            // Assert.Equal(Some("Value: 1"), result);
         }
         [Fact]
         public void Bind_None()
         {
             var result = None<string>()
                             // Uncomment the following once you've implemented Map
-                            .Bind(i => Some($"Value: {i}"))
+                            // .Bind(i => Some($"Value: {i}"))
                             ;
-            Assert.Equal(None<string>(), result);
+            // Assert.Equal(None<string>(), result);
         }
         [Fact]
         public void Bind_Nested()
         {
             var result_even = Some(2)
                             // Uncomment the following once you've implemented Map
-                            .Bind(i => i % 2 == 0 ? Some(i.ToString()) : None<string>())
+                            // .Bind(i => i % 2 == 0 ? Some(i.ToString()) : None<string>())
                             ;
-            Assert.Equal(Some("2"), result_even);
+            // Assert.Equal(Some("2"), result_even);
 
             var result_odd = Some(1)
                             // Uncomment the following once you've implemented Map
-                            .Bind(i => i % 2 == 0 ? Some(i.ToString()) : None<string>())
+                            // .Bind(i => i % 2 == 0 ? Some(i.ToString()) : None<string>())
                             ;
-            Assert.Equal(None<string>(), result_odd);
+            // Assert.Equal(None<string>(), result_odd);
         }
-
-        public static Option<int> ParseInt( string s){
-            int result = 0;
-            if (Int32.TryParse( s, out result)){
-                return Some(result);
-            } else{
-                return None<int>();
-            }
-        }
-
-        [Fact]
-        public void Bind_Other()
-        {
-            var a = "1";
-            var b = "2";
-            var c = "four";
-           
-          //if valid int, just add to previous value           
-            
-            var result_even = 
-                    Some(0)
-                    .Bind(i => ParseInt(a)
-                        .Bind(aa => 
-                            ParseInt(b)
-                            .Bind(bb => 
-                                ParseInt(c)
-                                    .Bind<int,int>(cc => Some(i + aa + bb + cc )))) )
-                    ;
-            
-            // var x = from i in Some(0)
-            //         from aa in ParseInt(a)
-            //         from bb in ParseInt(b)
-            //         from cc in ParseInt(c)
-            //         select i + aa + bb + cc;
-
-            Assert.Equal(Some(6), result_even);
-
-        }
-
     }
 }
